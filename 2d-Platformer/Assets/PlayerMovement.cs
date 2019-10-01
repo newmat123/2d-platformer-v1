ed05d96998af307ec948770ed3d738a01aeae84c
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2019-10-01 14:03:09 +0200

    Kommenteret kode

--- 2d-Platformer/Assets/PlayerMovement.cs
+++ 2d-Platformer/Assets/PlayerMovement.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    private float moveInput;
+    public float speed;             //Spillerens hastighed
+    public float jumpForce;         //Spillerens hoppe styrke
+    private float moveInput;        //Spillerens input
 
     private Rigidbody2D rb;
 
-    private bool isGrounded; //Checker om spilleren står på jorden
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
+    private bool isGrounded;        //Checker om spilleren står på jorden
+    public Transform groundCheck;   //Position som bruges til at tjekke om spilleren står på jorden
+    public float checkRadius;       //Radius som bruges til at tjekke om spilleren står på jorden
+    public LayerMask whatIsGround;  //Finder ud af hvilket lag der er jorden
 
-    private int extraJumps;
-    public int extraJumpsValue;
+    private int extraJumps;         
+    public int extraJumpsValue;     //Spillerens antal hop
   
 
     void Start()
@@ -27,27 +27,28 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);  //Checker om spilleren står på jorden
 
-        moveInput = Input.GetAxis("Horizontal"); //gør sådan at vi kan bruge A og D til at bevæge vores player 
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        moveInput = Input.GetAxis("Horizontal");                        //gør sådan at vi kan bruge A og D til at bevæge vores player 
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);    //Får spilleren til at bevæge sig
     }
 
     private void Update()
     {
-        if (isGrounded == true)
+        if (isGrounded == true) //Checker om objektet rammer laget "ground"
         {
             extraJumps = extraJumpsValue;
         }
 
-        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0)   //Hvis spilleren trykker på "W" og har flere hop
         {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
+           
+            rb.velocity = Vector2.up * jumpForce;    //Sørger for at spilleren kan hoppe
+            extraJumps--;                            //Trækker et jump fra
         }
-        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true) //Hvis spilleren trykker på "W" og kun har et hop
         {
-            rb.velocity = Vector2.up * jumpForce;
+            rb.velocity = Vector2.up * jumpForce;    //Sørger for at spilleren kan hoppe
         }
     }
 
