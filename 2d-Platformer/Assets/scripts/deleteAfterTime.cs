c51dfe4908fbac7153570a6218b48fba707ed97d
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-10-29 12:13:27 +0100

    popup text

---
+++ 2d-Platformer/Assets/scripts/deleteAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteAfterTime : MonoBehaviour
+{
+  
+    private float timer;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 3)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- 2d-Platformer/Assets/scripts/startScript.cs
+++ 2d-Platformer/Assets/scripts/startScript.cs
@@ -6,16 +6,19 @@ public class startScript : MonoBehaviour
 {
 
     public GameObject startMenu;
+    public GameObject text;
 
     void Start()
     {
         Time.timeScale = 0;
+        text.SetActive(false);
         startMenu.SetActive(true);
     }
 
     public void startGame1()
     {
         startMenu.SetActive(false);
+        text.SetActive(true);
         Time.timeScale = 1;
     }
 }
