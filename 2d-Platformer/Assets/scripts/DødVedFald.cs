6e155797d2ca901e7e6908cde8e41a29b9de72f1
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-10-29 09:20:07 +0100

    trigger

--- 2d-Platformer/Assets/scripts/DødVedFald.cs
+++ 2d-Platformer/Assets/scripts/DødVedFald.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class DødVedFald : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if(collision.gameObject.tag == "lava")
         {
 
